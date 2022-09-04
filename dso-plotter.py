00c9c87cd960dcfe4e13ebddcfc0af4738f8fac7
hanishkvc <hanishkvc@gmail.com>
2022-09-04 18:48:07 +0530

    Allow the plto data to be treated as signed or unsigned
    ie if required later

--- dso-plotter.py
+++ dso-plotter.py
@@ -26,7 +26,7 @@ def process_args(g, args):
 
 
 def parse_meta(g):
-    meta = array.array('h')
+    meta = array.array('h') # Need to check if all entries that is needed here correspond to 16bit signed values only or are there some unsigned 16bit values.
     meta.frombytes(g['meta'])
     print("Channel Volts/Div:")
     g['vdiv'] = []
@@ -42,15 +42,17 @@ def parse_meta(g):
 def plot_me(g):
     f = open(g['file'], "rb")
     d = f.read()
+    da = array.array("B") # control whether to treat as signed or unsigned
+    da.frombytes(d)
     g['meta'] = d[len(d)-512:]
     parse_meta(g)
     cd = np.zeros((4,4096))
     for i in range(0,4096*4,4):
         j = int(i/4)
-        cd[0,j] = d[i]
-        cd[1,j] = d[i+1]
-        cd[2,j] = d[i+2]
-        cd[3,j] = d[i+3]
+        cd[0,j] = da[i]
+        cd[1,j] = da[i+1]
+        cd[2,j] = da[i+2]
+        cd[3,j] = da[i+3]
     p = plt.subplots(4,1)
     for i in range(4):
         ax = p[1][i]
