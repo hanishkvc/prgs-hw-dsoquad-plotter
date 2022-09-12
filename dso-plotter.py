40611b66e965ebd1f8039f641d0fdc73199848f8
hanishkvc <hanishkvc@gmail.com>
2022-09-13 00:05:49 +0530

    Show delta wrt prev and cur mouse click positions

--- dso-plotter.py
+++ dso-plotter.py
@@ -208,8 +208,9 @@ def show_location(ev):
     xval = ev.xdata * g['tpixel']
     yval = g['yvB'] + (ev.ydata * g['yvPixel'])
     print(ev, xval, yval)
-    g['xytextprev'].set_text("{},{}".format(g['prevXVal'], g['prevYVal']))
-    g['xytextcur'].set_text("{},{}".format(xval, yval))
+    g['prevXYText'].set_text(": Prev:{},{}".format(g['prevXVal'], g['prevYVal']))
+    g['curXYText'].set_text(":  Cur:{},{}".format(xval, yval))
+    g['deltaXYText'].set_text(":Delta:{},{}".format(xval-g['prevXVal'], yval-g['prevYVal']))
     g['prevXVal'] = xval
     g['prevYVal'] = yval
     g['fig'].canvas.draw()
@@ -268,8 +269,9 @@ def plot_buffile(g):
     xlabels = friendly_times(xticks*g['tpixel'])
     ax.set_xticks(xticks, xlabels)
     ax.xaxis.set_minor_locator(MultipleLocator(30))
-    g['xytextprev'] = ax.text(0, 0.95, "", transform=ax.transAxes)
-    g['xytextcur'] = ax.text(0, 0.90, "", transform=ax.transAxes)
+    g['prevXYText'] = ax.text(0, 0.95, "", transform=ax.transAxes)
+    g['curXYText'] = ax.text(0, 0.90, "", transform=ax.transAxes)
+    g['deltaXYText'] = ax.text(0, 0.85, "", transform=ax.transAxes)
     g['prevXVal'] = 0
     g['prevYVal'] = 0
     plt.title(g['file'])
