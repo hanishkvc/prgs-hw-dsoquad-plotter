4d4d0478b913e45d76e8fa7cb20dfe8086204a41
hanishkvc <hanishkvc@gmail.com>
2022-09-14 01:37:21 +0530

    Add a test script to generate predefined midi messages

---
+++ scripts/test-midi-out.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Try send some predetermined midi messages
+# HanishKVC, 2022
+#
+
+import rtmidi
+import time
+
+
+mo = rtmidi.MidiOut()
+print(mo.get_ports())
+pid = input("Select the port to use:")
+pid = int(pid)
+mo.open_port(pid)
+
+for i in range(4096):
+    mo.send_message([0x90, 0x55, 0xaa])
+    time.sleep(0.001)
+    mo.send_message([0x80, 0x55, 0])
+    time.sleep(0.005)
